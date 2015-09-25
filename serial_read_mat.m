4512caf16dd4d7b4326d90979d5e794f7cd021ff
Shruti Iyer <shruti.iyer@students.olin.edu>
2015-09-25 10:40:19 -0400

    MATLAB-Arduino connection

--- serial_read_mat.m
+++ serial_read_mat.m
@@ -1,3 +1,36 @@
-s = serial('COM1');
-fopen(s)
-out = fscanf(s)
+clear all;
+
+s = serial('COM5');
+set(s, 'BaudRate', 9600);
+fopen(s);
+j = 0;
+theta = [];
+phi = [];
+position = [];
+receivingData = true;
+
+try
+    while(receivingData)
+        if s.BytesAvailable>=0
+            j = j+1;
+            serialLine = fscanf(s);
+            position = sscanf(serialLine, '%d','%d');
+            endOf = sscanf(serialLine, '%s'); 
+            %fprintf(,'%d');
+            
+            if strcmp(endOf, 'AWS')
+                receivingData = false;
+            else
+                theta(j) = position(2);
+                phi(j) = position(1);
+            end
+        end
+    end
+    fclose(s);
+    delete(s);
+    clear s
+catch
+    fclose(s);
+    delete(s);
+    clear s
+end
